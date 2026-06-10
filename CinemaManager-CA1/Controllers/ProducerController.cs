ed333aac33ec3663b63424826c1ff2e28766499e
firas1996 <firas.hm.1996@gmail.com>
2026-06-10 18:40:06 +0100

    Add project files.

---
+++ CinemaManager-CA1/Controllers/ProducerController.cs
@@ -0,0 +1,90 @@
+﻿using CinemaManager_CA1.Models.Cinema;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaManager_CA1.Controllers
+{
+    public class ProducerController : Controller
+    {
+        CinemaDbCa1Context _context;
+        public ProducerController(CinemaDbCa1Context context)
+        {
+            _context = context;
+        }
+        // GET: ProducersController
+        public ActionResult Index()
+        {
+            //ViewBag.producers = _context.Producers.ToList() ;
+            return View(_context.Producers.ToList());
+        }
+
+        // GET: ProducersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProducersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProducersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProducersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProducersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProducersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProducersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
